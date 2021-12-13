c982ae4ff82998ee372e1af6be54732bb11305f8
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2021-12-13 09:04:07 -0500

    Add files via upload

--- Study-App/study.js
+++ Study-App/study.js
@@ -7,14 +7,8 @@ function myFunction() {
 }
 
 
-// Title Change
 
-const input = document.querySelector('input');
-const log = document.getElementById('values');
 
-input.addEventListener('input', updateValue);
 
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
+
 
